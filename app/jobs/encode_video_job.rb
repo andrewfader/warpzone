e6e76c635c7b1a341b25cd0c831c2517a44f519c
Andrew Fader <fader@yagni.co>
2017-05-05 21:55:10 -0400

    video file tweaks

--- app/jobs/encode_video_job.rb
+++ app/jobs/encode_video_job.rb
@@ -5,20 +5,23 @@ class EncodeVideoJob < ApplicationJob
     webm = video.file.file.file
     movie = FFMPEG::Movie.new(webm)
     if movie.valid?
-      filename = "#{video.id}_1.gif"
-      video.filepath = filename
+      filename = "#{video.id}-#{Time.now.to_i}.gif"
       gif = "#{Rails.root.join("public/uploads/video/file/#{video.id}/#{filename}")}"
       palette = "/tmp/palette.png"
-      filters = "fps=15,scale=320:-1:flags=lanczos"
+      filters = "fps=15,scale=720:-1:flags=lanczos"
       command = "ffmpeg -v warning -i #{webm} -vf \"#{filters},palettegen\" -y #{palette}"
-      command2= "ffmpeg -v warning -i #{webm} -t 00:00:05 -i #{palette} -lavfi \"#{filters} [x]; [x][1:v] paletteuse\" -y #{gif}"
+      command2= "ffmpeg -v warning -i #{webm} -t 00:00:08 -i #{palette} -lavfi \"#{filters} [x]; [x][1:v] paletteuse\" -y #{gif}"
+      command3= "convert #{gif} -delay 15 -loop 0 #{gif}"
 
       puts command
       puts Open3.popen3(command)
       sleep 4
       puts command2
-      puts Open3.popen3(command2)
+      puts Open3.popen3("#{command2}")
+      sleep 8
+      puts Open3.popen3("#{command3}")
       sleep 4
+      video.filepath = filename
       video.save
     end
   end
